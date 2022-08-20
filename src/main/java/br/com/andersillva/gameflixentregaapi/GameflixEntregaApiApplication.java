ae435f17e25d2c31f4f9abcb63423a2df3993463
Ander Sillva <andersillva11@gmail.com>
2022-08-20 14:50:26 -0300

    Criação do repositório.

---
+++ src/main/java/br/com/andersillva/gameflixentregaapi/GameflixEntregaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.andersillva.gameflixentregaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameflixEntregaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameflixEntregaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andersillva/gameflixentregaapi/GameflixEntregaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andersillva.gameflixentregaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameflixEntregaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
